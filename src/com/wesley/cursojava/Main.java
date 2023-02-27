246dc2c5dba5db096bfbf63192bf104ff88671c7
Wesley <wesley55daniel@gmail.com>
2023-02-27 19:58:27 -0300

    Primeiro commit

---
+++ src/com/wesley/cursojava/Main.java
@@ -0,0 +1,10 @@
+package com.wesley.cursojava;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/com/wesley/cursojava/Professor.java
@@ -0,0 +1,31 @@
+package com.wesley.cursojava;
+
+public class Professor extends Pessoa {
+	
+	
+	private double salario;
+	private String nomeCurso;
+	
+	
+	public double calcularSalarioLiquido() {
+		
+		return 0;
+	}
+	
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	
+
+}
